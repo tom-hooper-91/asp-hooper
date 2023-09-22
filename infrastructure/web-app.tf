f3990b9cac71c352b2ec064221bf61e67e8083f3
Tom Hooper <tom.hooper@spektrix.com>
2023-09-22 14:32:16 +0100

    Rename resources in web-app.tf

--- infrastructure/web-app.tf
+++ infrastructure/web-app.tf
@@ -1,21 +1,23 @@
-resource "azurerm_resource_group" "web-app-rg" {
-  name     = "hoopers-webapp-1_group"
+resource "azurerm_resource_group" "web_app_rg" {
+  name     = "tom_hoopers_web_app_group"
   location = "West Europe"
 }
 
-resource "azurerm_service_plan" "web-app-plan" {
-  name                = "ASP-hooperswebapp1group-a138"
-  resource_group_name = azurerm_resource_group.web-app-rg.name
-  location            = azurerm_resource_group.web-app-rg.location
+resource "azurerm_service_plan" "web_app_plan" {
+  name                = "hoopers_service_plan"
+  resource_group_name = azurerm_resource_group.web_app_rg.name
+  location            = azurerm_resource_group.web_app_rg.location
   os_type             = "Linux"
   sku_name            = "F1"
 }
 
-resource "azurerm_linux_web_app" "web-app-1" {
-  name                = "hoopers-webapp-1"
-  resource_group_name = azurerm_resource_group.web-app-rg.name
-  location            = azurerm_service_plan.web-app-plan.location
-  service_plan_id     = azurerm_service_plan.web-app-plan.id
+resource "azurerm_linux_web_app" "web_app" {
+  name                = "hoopers-web-app"
+  resource_group_name = azurerm_resource_group.web_app_rg.name
+  location            = azurerm_service_plan.web_app_plan.location
+  service_plan_id     = azurerm_service_plan.web_app_plan.id
 
-  site_config {}
+  site_config {
+    always_on = false
+  }
 }
