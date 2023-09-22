08336d9402c5f314d5a3f6c03ed732ff8c4fb9e4
Tom Hooper <tom.hooper@spektrix.com>
2023-09-22 16:54:57 +0100

    WIP configure GitHub Actions in terraform

--- infrastructure/web-app.tf
+++ infrastructure/web-app.tf
@@ -19,5 +19,40 @@ resource "azurerm_linux_web_app" "web_app" {
 
   site_config {
     always_on = false
+    application_stack {
+        dotnet_version = "7.0"
+    }
   }
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [
+        // Github? https://stackoverflow.com/questions/75851651/repository-updatesitesourcecontrol-operation-failed-with-microsoft-web-hosting
+    ]
+  }
+}
+
+resource "azurerm_app_service_source_control" "source_control" {
+  app_id   = azurerm_linux_web_app.web_app.id
+  repo_url = "https://github.com/tom-hooper-91/asp-hooper"
+  branch   = "main"
+
+  github_action_configuration {
+    generate_workflow_file = true
+    code_configuration {
+        runtime_stack = "dotnetcore"
+        runtime_version = "7.0"
+    }
+  }
+}
+
+variable "github_auth_token" {
+  type        = string
+  description = "Github Auth Token from Github > Developer Settings > Personal Access Tokens > Tokens Classic (needs to have repo permission)"
+}
+
+resource "azurerm_source_control_token" "source_control_token" {
+  type         = "GitHub"
+  token        = var.github_auth_token
+  token_secret = var.github_auth_token
 }
