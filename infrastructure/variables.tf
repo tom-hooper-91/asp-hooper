650738f3148ee34d9719d01fdd4e5b8bb0d14748
Tom Hooper <tom.hooper@spektrix.com>
2023-11-17 10:22:36 +0000

    Move variabales into own file

---
+++ infrastructure/variables.tf
@@ -0,0 +1,4 @@
+variable "github_auth_token" {
+  type        = string
+  description = "Github Auth Token from Github > Developer Settings > Personal Access Tokens > Tokens Classic (needs to have repo permission)"
+}
